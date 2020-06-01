70ac9dd28787dbc6bd9048e681418aa1468b5215
ParallelsMat <matcursos73@gmail.com>
2020-05-31 20:41:02 -0400

    Segundo commit comtemplando a criação das classes professores e disciplinas

--- venv/controle/core/admin.py
+++ venv/controle/core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from core.models import Professor, Disciplina
 
 # Register your models here.
+
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = ('Nome', 'Exeperiencia_profissional', 'Disponibilidade')
+
+class DisciplinaAdmin(admin.ModelAdmin):
+    list_display = ('Nome_Disciplina', 'carga_horaria')
+admin.site.register(Professor, ProfessorAdmin)
+admin.site.register(Disciplina, DisciplinaAdmin)
--- venv/controle/core/models.py
+++ venv/controle/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Professor(models.Model):
+    Nome = models.CharField(max_length=100)
+    codigo_prof = models.CharField(max_length=10)
+    Exeperiencia_profissional = models.CharField('experiencia', max_length=200)
+    Hora_aula = models.TimeField('Tempo', auto_now=True)
+    Disponibilidade = models.TextField(max_length=150)
+
+class Disciplina(models.Model):
+    Nome_Disciplina = models.TextField(max_length=50)
+    codigo_disciplina = models.CharField(max_length=10)
+    carga_horaria = models.TimeField()
+
+
+
