310b032af876c606bba88c9a34d90ea20efb15e8
ParallelsMat <matcursos73@gmail.com>
2020-06-07 19:50:13 -0400

    Terceiro commit da aplicação, contemplando a criação da tabela Professor_disciplinas que contém as chaves estrangeiras.

--- venv/controle/core/admin.py
+++ venv/controle/core/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from core.models import Professor, Disciplina
+from core.models import Professor, Disciplina, Professor_Disciplina
 
 # Register your models here.
 
 class ProfessorAdmin(admin.ModelAdmin):
     list_display = ('Nome', 'Exeperiencia_profissional', 'Disponibilidade')
+admin.site.register(Professor , ProfessorAdmin)
 
 class DisciplinaAdmin(admin.ModelAdmin):
     list_display = ('Nome_Disciplina', 'carga_horaria')
-admin.site.register(Professor, ProfessorAdmin)
 admin.site.register(Disciplina, DisciplinaAdmin)
+
+class Professor_DisciplinaAdmin(admin.ModelAdmin):
+    list_display = ('nome_curso', 'semestre')
+admin.site.register(Professor_Disciplina, Professor_DisciplinaAdmin)
--- venv/controle/core/models.py
+++ venv/controle/core/models.py
@@ -4,15 +4,19 @@ from django.db import models
 
 class Professor(models.Model):
     Nome = models.CharField(max_length=100)
-    codigo_prof = models.CharField(max_length=10)
+    codigo_prof = models.DecimalField(max_digits=4, decimal_places=0)
     Exeperiencia_profissional = models.CharField('experiencia', max_length=200)
     Hora_aula = models.TimeField('Tempo', auto_now=True)
-    Disponibilidade = models.TextField(max_length=150)
+    Disponibilidade = models.TextField(max_length=100)
 
 class Disciplina(models.Model):
-    Nome_Disciplina = models.TextField(max_length=50)
-    codigo_disciplina = models.CharField(max_length=10)
+    Nome_Disciplina = models.TextField(max_length=25)
+    codigo_disciplina = models.DecimalField(max_digits=4, decimal_places=0)
     carga_horaria = models.TimeField()
 
-
+class Professor_Disciplina(models.Model):
+    codigo_prof = models.DecimalField(max_digits=4, decimal_places=0)
+    codigo_disciplina = models.DecimalField(max_digits=4, decimal_places=0)
+    nome_curso = models.CharField(max_length=20)
+    semestre = models.DecimalField(max_digits=4, decimal_places=0)
 
